4dc66e356269a980a42144cb52be22e99504973e
DevSecOpsGit <DevSecOpsGit@users.noreply.github.com>
2022-02-12 06:44:05 +0000

    deploy: f542beb7cfc80e01a25477111966b2ba4bbadf43

---
+++ _nuxt/static/1644648240/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fblog\u002F_nuxt\u002Fstatic\u002F1644648240",layout:"home",error:a,serverRendered:b,routePath:"\u002Fcontact",config:{_app:{basePath:"\u002Fblog\u002F",assetsPath:"\u002Fblog\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"915b9f1d"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
